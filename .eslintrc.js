2b1513c785d713fde5900432aa8d664b1cde3447
Luiz Carlos Francisco Junior <lcfjunior04@gmail.com>
2024-06-28 20:10:46 -0300

    aaaaaaaA

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parserOptions: {
+      ecmaVersion: 2023, // ou versão compatível com seus recursos ECMAScript
+      sourceType: 'module',
+    },
+    // Outras configurações do ESLint aqui...
+  };
+  
