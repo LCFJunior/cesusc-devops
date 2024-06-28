acb883dab0781c5c0835a3b3702e5b0d9aa7389d
Luiz Carlos Francisco Junior <129459925+LCFJunior@users.noreply.github.com>
2024-06-28 20:37:07 -0300

    Delete eslint.config.mjs

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs", globals: { node: true } } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-];
