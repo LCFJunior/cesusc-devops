7d180fc4d9ef3306563496af766a4373b2f621d4
Luiz Carlos Francisco Junior <lcfjunior04@gmail.com>
2024-06-28 20:12:15 -0300

    aaaaaaa

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 // Serve static files from the 'views' directory
-const viewsPath = new URL('.', import.meta.url).pathname;
+const viewsPath = __dirname;
 app.use(express.static(path.join(viewsPath, 'views')));
 
 // Serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(viewsPath, 'views', 'index.html'));
 });
-
 
 if (require.main === module) {
     app.listen(PORT, '0.0.0.0', () => {
