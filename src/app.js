782f7f9cfe842516a28a8c9fa5a7e957d44a3777
Luiz Carlos Francisco Junior <lcfjunior04@gmail.com>
2024-06-28 04:39:15 -0300

    att JEST

--- src/app.js
+++ src/app.js
@@ -4,18 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Simula a variável __dirname
-const __dirname = require('path').resolve();
-
-// Define o diretório de arquivos estáticos para o Express
 app.use(express.static(path.join(__dirname, 'views')));
 
-// Rota para servir o arquivo index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Inicia o servidor
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+module.exports = app;
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
+}
