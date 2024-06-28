ad0735947b6af52a900c5f66b0bc59e404adb131
Luiz Carlos Francisco Junior <lcfjunior04@gmail.com>
2024-06-28 04:21:34 -0300

    att ESLINT

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const app = express();
 const PORT = 3000;
 
-let test
+// Simula a variável __dirname
+const __dirname = require('path').resolve();
 
+// Define o diretório de arquivos estáticos para o Express
+app.use(express.static(path.join(__dirname, 'views')));
+
+// Rota para servir o arquivo index.html
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html' )
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// Inicia o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
