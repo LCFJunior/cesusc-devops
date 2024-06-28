7689749d47a47a61d7b315ef9e7c2d5dfce81d8e
Luiz Carlos Francisco Junior <lcfjunior04@gmail.com>
2024-06-28 14:09:26 -0300

    att test

--- src/app.js
+++ src/app.js
@@ -4,17 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Define o diretório de arquivos estáticos para o Express
 app.use(express.static(path.join(__dirname, 'views')));
 
-// Rota para servir o arquivo index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-module.exports = app; // Exporta o aplicativo Express
+module.exports = app;
 
-// Inicia o servidor (opcionalmente, dependendo de como você está gerenciando isso)
 if (require.main === module) {
     app.listen(PORT, '0.0.0.0', () => {
         console.log(`Servidor rodando em http://0.0.0.0:${PORT}`);
