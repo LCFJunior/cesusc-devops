8daa788a8131d9bca8f28bc61df426a37a058f84
Luiz Carlos Francisco Junior <lcfjunior04@gmail.com>
2024-06-28 20:14:48 -0300

    aaaaaaaA

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,20 @@ module.exports = {
       ecmaVersion: 2023, // ou versão compatível com seus recursos ECMAScript
       sourceType: 'module',
     },
+    env: {
+        // Defina o ambiente como Node.js para reconhecer variáveis globais do Node.js
+        node: true,
+    },
+
+    globals: {
+        // Adicione __dirname como uma variável global
+        __dirname: 'readonly',
+    },
+
+    // Adicione o plugin node ao conjunto de plugins
+    plugins: [
+        'node',
+    ],
     // Outras configurações do ESLint aqui...
   };
   
